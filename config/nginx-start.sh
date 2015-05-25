8550a25eb6a6b08e210b752432520f53cbef944b
Zak Henry <zak.henry@gmail.com>
2015-05-26 09:57:36 +1000

    Updated port read from env to match docker-compose naming

--- config/nginx-start.sh
+++ config/nginx-start.sh
@@ -4,5 +4,5 @@ cp /opt/etc/nginx.conf /etc/nginx/nginx.conf
 cp /data/${NGINX_VHOSTS} /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/*.conf /etc/nginx/sites-enabled/
 
-sed -i "s/%fpm-ip%/$FPM_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
+sed -i "s/%fpm-ip%/$PHP_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
 exec /usr/sbin/nginx
