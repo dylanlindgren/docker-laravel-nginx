339bb53b3c3aec8044b48613ea942cb27d8587d1
Zak Henry <zak.henry@gmail.com>
2015-07-18 18:35:27 +1000

    Fixed sed replacements of %data-root%

--- config/nginx-start.sh
+++ config/nginx-start.sh
@@ -5,5 +5,7 @@ cp ${DATA_ROOT:-/data}/${NGINX_VHOSTS} /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/*.conf /etc/nginx/sites-enabled/
 
 sed -i "s/%fastgci-ip%/$PHP_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
-sed -i "s/%data-root%/${DATA_ROOT:-/data}/" /etc/nginx/nginx.conf
+sed -i "s|%data-root%|${DATA_ROOT:-/data}|" /etc/nginx/nginx.conf
+sed -i "s|%data-root%|${DATA_ROOT:-/data}|" /etc/nginx/sites-available/*.conf
+
 exec /usr/sbin/nginx
