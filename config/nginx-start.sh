bba0efcd590c62ad1de2040947ff35c5396350bd
Zak Henry <zak.henry@gmail.com>
2015-07-20 23:43:19 +1000

    Configured startup script to iterate through the env vars doing replacements on the vhost files.

--- config/nginx-start.sh
+++ config/nginx-start.sh
@@ -8,4 +8,14 @@ sed -i "s/%fastgci-ip%/$PHP_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
 sed -i "s|%data-root%|${DATA_ROOT:-/data}|" /etc/nginx/nginx.conf
 sed -i "s|%data-root%|${DATA_ROOT:-/data}|" /etc/nginx/sites-available/*.conf
 
+#iterate though all the env vars and replace %ENV% with the value
+printenv | while read envdef; do #iterate over all env vars
+    key=${envdef%=*} # extract the env key name
+#    echo "%"$env"% = "${!env};
+
+    sed -i "s|%$key%|${!key}|" /etc/nginx/sites-available/*.conf # replace all instances of %KEY% with the value
+done
+
+cat /etc/nginx/sites-available/*.conf
+
 exec /usr/sbin/nginx
