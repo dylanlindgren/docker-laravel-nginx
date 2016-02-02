1a6d24b21cb1e9714a571901acea6c02fe0f95d5
Zak Henry <zak.henry@gmail.com>
2016-02-02 17:34:26 +1100

    Simplified docker file to base on debian:jessie and not care about data root

--- config/nginx-start.sh
+++ config/nginx-start.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-cp /opt/etc/nginx.conf /etc/nginx/nginx.conf
+NGINX_VHOSTS=${NGINX_VHOSTS:-/data/vhosts}
 
-export DATA_ROOT=${DATA_ROOT:-/data}
-
-cp ${DATA_ROOT}/${NGINX_VHOSTS} /etc/nginx/sites-available/
+rm /etc/nginx/sites-available/* /etc/nginx/sites-enabled/*
+cp $NGINX_VHOSTS /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/*.conf /etc/nginx/sites-enabled/
 
-#export the current tcp ip address so configuration can forward the nginx service port to dependent services
-export UPSTREAM_WEB_TCP_ADDR=$(ip -f inet -o addr show eth0 | cut -d\  -f 7 | cut -d/ -f 1)
 #iterate though all the env vars and replace %ENV% with the value
 printenv | while read envdef; do #iterate over all env vars
     key=${envdef%=*} # extract the env key name
-#    echo "%"$env"% = "${!env};
 
     # replace all instances of %KEY% with the relevant environment value
     sed -i "s|%$key%|${!key}|" /etc/nginx/sites-available/*.conf
